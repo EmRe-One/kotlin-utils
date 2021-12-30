c3bfe2068a78ae4cb8f2775339dd49cc64263cf1
Emre Akgül <emre.akguel.1@gmail.com>
2021-12-30 04:11:34 +0100

    add prepare next day task for AoC

---
+++ template/DayX.kt
@@ -0,0 +1,14 @@
+package de.emreak.adventofcode.days
+
+object Day$1 {
+
+    fun part1(input: List<String>): Int {
+
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+
+        return 0
+    }
+}
